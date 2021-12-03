31b0f1c7d3355a48832fec3df1fa91f1df52e126
Samuelsson, L (Lars) <SamuelssonL@delagelanden.com>
2021-12-03 12:36:04 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("What is yoru name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        }
+    }
+}
